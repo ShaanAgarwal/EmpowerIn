1b8f9ea65af504e023cff484fe24df1cced77b82
ShaanAgarwal <shaanagarwalofficial@gmail.com>
2024-01-13 00:28:07 +0530

    Added The Basic Route and Page Of ContactUs Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./Pages/HomePage/HomePage";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import AboutUsPage from "./Pages/AboutUsPage/AboutUsPage";
 import ServicesPage from "./Pages/ServicesPage/ServicesPage";
+import ContactUsPage from "./Pages/ContactUsPage/ContactUsPage";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/aboutUs" element={<AboutUsPage />} />
         <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contactUs" element={<ContactUsPage />} />
       </Routes>
     </BrowserRouter>
   );
