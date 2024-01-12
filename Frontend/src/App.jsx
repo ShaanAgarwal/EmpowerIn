c1ad6bbc093e2443d36712d0ec8019fa4030fbc0
ShaanAgarwal <shaanagarwalofficial@gmail.com>
2024-01-13 00:16:44 +0530

    Added The Basic Route and Page For About Us Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import LoginPage from "./Pages/LoginPage/LoginPage";
+import AboutUsPage from "./Pages/AboutUsPage/AboutUsPage";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/aboutUs" element={<AboutUsPage />} />
       </Routes>
     </BrowserRouter>
   );
