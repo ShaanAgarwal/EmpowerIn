e3933a686d62004779345c763ee2036175ef6d6e
ShaanAgarwal <shaanagarwalofficial@gmail.com>
2024-01-13 00:07:10 +0530

    Created Test Login User Function In Backend Directory

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require("../Models/userSchema");
 
 const testUser = async (req, res) => {
@@ -27,9 +28,10 @@ const testUserRegister = async (req, res) => {
         if (!userType) {
             return res.status(400).json({ message: "User Type is required.", success: false });
         };
-        if (userType != ('Admin' || 'HeadHR' || 'HR' || 'Candidate')) {
-            return res.status(400).json({ message: "Invalid value for User Types", success: false });
-        };
+        const allowedUserTypes = ["Admin", "HeadHR", "HR", "Candidate"];
+        if (!allowedUserTypes.includes(userType)) {
+            return res.status(400).json({ message: "Invalid value for User Type", success: false });
+        }
         const existingUserEmail = await User.findOne({ email: email });
         if (existingUserEmail) {
             return res.status(409).json({ message: "Email already exists.", success: false });
@@ -37,11 +39,36 @@ const testUserRegister = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newTestUser = new User({ firstName: firstName, lastName: lastName, email: email, userType: userType, password: hashedPassword });
         await newTestUser.save();
-        return res.status(200).json({ message: "Successful in creating a new Test User", success: true, newTestUser });
+        return res.status(201).json({ message: "Successful in creating a new Test User", success: true, newTestUser });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Internal Server Error", success: false });
     };
 };
 
-module.exports = { testUser, testUserRegister };
+const testuserLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email) {
+            return res.status(400).json({ message: "Email is required", success: false });
+        };
+        if (!password) {
+            return res.status(400).json({ message: "Password is required", success: false });
+        };
+        const userExist = await User.findOne({ email: email });
+        if (!userExist) {
+            return res.status(400).json({ message: "User with given email does not exist.", success: false });
+        };
+        const passwordMatch = await bcrypt.compare(password, userExist.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ message: "Incorrect password", success: false });
+        };
+        const token = jwt.sign({ userId: userExist._id, email: userExist.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        return res.status(200).json({ message: "The user has Logged in successfully", success: true, userExist, token });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error", success: false });
+    };
+};
+
+module.exports = { testUser, testUserRegister, testuserLogin };
--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { testUserRegister, testUser } = require('../Controllers/userController');
+const { testUserRegister, testUser, testuserLogin } = require('../Controllers/userController');
 const router = express.Router();
 
 router.get("/", testUser);
 router.post("/testUserRegister", testUserRegister);
+router.post("/userLogin", testuserLogin);
 
 module.exports = router;
