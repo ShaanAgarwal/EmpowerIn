3c2fa70fd576b2c9df09ebed402b64210adc6509
ShaanAgarwal <shaanagarwalofficial@gmail.com>
2024-01-31 19:36:25 +0530

    Initial

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,9 @@ import AboutUsPage from "./Pages/AboutUsPage/AboutUsPage";
 import ServicesPage from "./Pages/ServicesPage/ServicesPage";
 import ContactUsPage from "./Pages/ContactUsPage/ContactUsPage";
 import ContactUsResponsePage from "./Pages/ContactUsResponsePage/ContactUsResponsePage";
+import ForgotPasswordEnterEmailPage from "./Pages/ForgotPasswordFlow/ForgotPasswordEnterEmailPage/ForgotPasswordEnterEmailPage";
+import ForgotPasswordEnterOTPPage from "./Pages/ForgotPasswordFlow/ForgotPasswordEnterOTPPage/ForgotPasswordEnterOTPPage";
+import ForgotPasswordResetPasswordPage from "./Pages/ForgotPasswordFlow/ForgotPasswordResetPasswordPage/ForgotPasswordResetPasswordPage";
 
 const App = () => {
   return (
@@ -16,6 +19,9 @@ const App = () => {
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/contactUs" element={<ContactUsPage />} />
         <Route path="/contactUsResponse" element={<ContactUsResponsePage />} />
+        <Route path="/forgotPassword-enterEmail" element={<ForgotPasswordEnterEmailPage />} />
+        <Route path="/forgotPassword-enterOTP" element={<ForgotPasswordEnterOTPPage />} />
+        <Route path="/forgotPassword-resetPassword" element={<ForgotPasswordResetPasswordPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ Frontend/src/Pages/ForgotPasswordFlow/ForgotPasswordResetPasswordPage/ForgotPasswordResetPasswordPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./ForgotPasswordResetPasswordPage.css";
+
+const ForgotPasswordResetPasswordPage = () => {
+  return (
+    <>
+    <div>ForgotPasswordResetPasswordPage</div>
+    </>
+  );
+};
+
+export default ForgotPasswordResetPasswordPage;
