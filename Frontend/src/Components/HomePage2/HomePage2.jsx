37fbbd634b46bd11818b064f17a8bc8f2fc04492
YashBur <yashburad2611@gmail.com>
2024-02-03 19:32:17 +0530

    hometwo added

---
+++ Frontend/src/Components/HomePage2/HomePage2.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./HomePage2.css";
+function HomePage2() {
+  return (
+    
+        <div className="home2">
+            <div className="left-text">
+                <div className="first-line">Authentic IT Solutions</div>
+                <div className="second-line">Streamlined IT Solutions: Effortless Problem Resolution, Every Day.</div>
+            </div>
+            <div className="right-small-images">
+                <div className="first-row-img">
+                    <div className="img1"><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1706966222/cloud-data_1_a3oesy.png" alt="cloud"></img></div>
+                    <div className="img2"><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1706966258/cyber-security_1_hyuzjm.png" alt="cyber-security"></img></div>
+                    <div className="img3"><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1706966322/image_3_jlgiaz.png" alt=""></img></div>
+                </div>
+                <div className="second-row-img">
+                    <div className="img4"><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1706966388/image_4_y7xmza.png" alt=""></img></div>
+                    <div className="img5"><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1706966443/image_5_d67j0f.png" alt=""></img></div>
+                    <div className="img6"><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1706966495/image_6_ujpwgt.png" alt=""></img></div>
+                </div>
+            </div>
+        </div>
+
+  )
+}
+
+export default HomePage2
--- Frontend/src/Pages/HomePage/HomePage.jsx
+++ Frontend/src/Pages/HomePage/HomePage.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import NavBar from "../../Components/NavBar/NavBar";
 import Footer from "../../Components/Footer/Footer";
 import HomePageTop from "../../Components/HomePageTop/HomePageTop";
+import HomePage2 from "../../Components/HomePage2/HomePage2";
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
       <HomePageTop />
+      <HomePage2 />
       <Footer />
+      
     </>
   );
 };
