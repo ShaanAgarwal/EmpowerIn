ccf286d2764a7252e68211ab006b782d76162460
YashBur <yashburad2611@gmail.com>
2024-02-09 13:04:39 +0530

    Created About Page Design

---
+++ Frontend/src/Components/AboutUs/AboutUs.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import "./AboutUS.css";
+function AboutUs() {
+  return (
+
+    <div class="about-container">
+    <div class="about-top">
+        <div class="about-heading">About Us</div>
+        <div class="about-para">We are your dedicated partners in innovation, committed to delivering forward-thinking solutions. Our passion for technology drives us to create impactful and tailored experiences that empower businesses to thrive in the digital era.</div>
+    </div>
+
+    <div class="about-middle">
+        <div class="preparing-line">
+            Preparing for a better tomorrow
+        </div>
+        <div class="yellow-line">
+            <img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1707039682/Vector_euhawx.png" alt="yellowLine"></img>
+        </div>
+        <div class="firstmiddle">
+            <div class="left-firstmiddle">
+              <div class="heading-middle1">Digital Transformation Solutions</div>
+              <div class="subheading1">Ignite progress with our transformative IT solutions, shaping a future where innovation is at the heart of your digital journey.</div>
+              <div><button class="view-more1">View More</button></div>
+            </div>
+            <div class="right-firstmiddle">
+                <div><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1706991617/image_15_qa39ec.png" alt="image1" class="middle-img1"></img></div>
+            </div>
+        </div>
+        <div class="secondmiddle">
+            <div class="left-secondmiddle">
+                <div><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1706991656/image_16_rrsxwt.png" alt="image1" class="middle-img2"></img></div>
+            </div>
+            <div class="right-secondmiddle">
+                <div class="heading-middle2">Cybersecurity Expertise</div>
+                <div class="subheading2">Empower your defenses with our cybersecurity expertise, safeguarding your digital assets with cutting-edge protection.</div>
+                <div><button class="view-more2">View More</button></div>
+            </div>                    
+        </div>
+        <div class="thirdmiddle">
+            <div class="left-thirdmiddle">
+                <div class="heading-middle3">Strategic IT Consulting</div>
+                <div class="subheading3">Chart your course to success with our strategic IT consulting, guiding you through the dynamic landscape of technology-driven opportunities.</div>
+                <div><button class="view-more3">View More</button></div>
+              </div>
+              <div class="right-thirdmiddle">
+                  <div><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1706991724/image_17_qny12s.png" alt="image1" class="middle-img3"></img></div>
+              </div>
+        </div>
+        
+    </div>
+
+    <div class="why-it-works">
+        <div class="why-works-line">Why it works</div>
+        <div class="yellow-line">
+            <img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1707039682/Vector_euhawx.png" alt="yellowLine"></img>
+        </div>
+        <div class="three-cards">
+            <div class="card1">
+                <div class="image1"><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1707041600/Frame_jifbmm.png"></img></div>
+                <div class="heading-card1">Efficiency Unleashed</div>
+                <div class="subheading-card1">Experience streamlined workflows and optimized processes that make your business operations more efficient and productive</div>
+
+            </div>
+            <div class="card2">
+                <div class="image2"><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1707041653/Frame_1_n7k5ix.png"></img></div>
+                <div class="heading-card2">Innovation at Every Level</div>
+                <div class="subheading-card2">Discover how our IT solutions drive innovation, bringing fresh perspectives to every aspect of your digital landscape</div>
+            </div>
+            <div class="card3">
+                <div class="image3"><img src="https://res.cloudinary.com/dijg8bdwk/image/upload/v1707041680/Group_yiczfa.png"></img></div>
+                <div class="heading-card3">Tailored for Your Success</div>
+                <div class="subheading-card3">Our approach is personalized and precise, ensuring that our IT solutions are crafted to meet your unique business goals and aspirations</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+  )
+}
+
+export default AboutUs
--- Frontend/src/Pages/AboutUsPage/AboutUsPage.jsx
+++ Frontend/src/Pages/AboutUsPage/AboutUsPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import NavBar from "../../Components/NavBar/NavBar";
 import Footer from "../../Components/Footer/Footer";
+import AboutUs from "../../Components/AboutUs/AboutUs";
 
 const AboutUsPage = () => {
   return (
     <>
       <NavBar />
-      <div>AboutUsPage</div>
+      <AboutUs />
       <Footer />
     </>
   );
