aef04e6332c3fe3c341545a2e40f075bac453be8
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-02-24 23:52:14 +0530

    GET request get audit log for forgot password path

--- Backend/Controllers/auditLogController.js
+++ Backend/Controllers/auditLogController.js
@@ -1,5 +1,9 @@
 const RegisterCandidateAPI = require("../Models/Audit Logs/Candidate Controller/registerCandidateAPI");
 const VerifyOTPRegistrationAPI = require("../Models/Audit Logs/Candidate Controller/verifyOTPRegistrationAPI");
+const ContactUsEmailAPI = require("../Models/Audit Logs/Contact Us Controller/contactUsEmailAPI");
+const RegisterEmailForgotPasswordAPI = require("../Models/Audit Logs/Forgot Password Controller/registerEmailForgotPasswordAPI");
+const VerifyOTPForgotPasswordAPI = require("../Models/Audit Logs/Forgot Password Controller/verifyOTPForgotPasswordAPI");
+const PasswordResetForgotPasswordAPI = require("../Models/Audit Logs/Forgot Password Controller/passwordResetForgotPasswordAPI");
 
 const getRegisterCandidateAPIAuditLogs = async (req, res) => {
     try {
@@ -14,11 +18,51 @@ const getRegisterCandidateAPIAuditLogs = async (req, res) => {
 const verifyOTPRegistrationAPI = async (req, res) => {
     try {
         const verifyOTPRegistrationAPI = await VerifyOTPRegistrationAPI.find();
-        return res.status(200).json({ message: verifyOTPRegistrationAPI, success: false });
+        return res.status(200).json({ message: verifyOTPRegistrationAPI, success: true });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Internal Server Error", success: false });
     };
 };
 
-module.exports = {getRegisterCandidateAPIAuditLogs, verifyOTPRegistrationAPI};
+const contactUsEmailAPI = async (req, res) => {
+    try {
+        const contactUsEmailAPI = await ContactUsEmailAPI.find();
+        return res.status(200).json({ message: contactUsEmailAPI, success: true });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error", success: false });
+    };
+};
+
+const registerEmailForgotPasswordAPI = async (req, res) => {
+    try {
+        const registerEmailForgotPasswordAPI = await RegisterEmailForgotPasswordAPI.find();
+        return res.status(200).json({ message: registerEmailForgotPasswordAPI, success: true });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error", success: false });
+    };
+};
+
+const verifyOTPForgotPasswordAPI = async (req,res) => {
+    try {
+        const verifyOTPForgotPasswordAPI = await VerifyOTPForgotPasswordAPI.find();
+        return res.status(200).json({message: verifyOTPForgotPasswordAPI, success: true});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal Server Error", success: false});
+    };
+};
+
+const passwordResetForgotPasswordAPI = async (req,res) => {
+    try {
+        const passwordResetForgotPasswordAPI = await PasswordResetForgotPasswordAPI.find();
+        return res.status(200).json({message: passwordResetForgotPasswordAPI, success: true});
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({message: "Internal Server Error", success: false});
+    };
+};
+
+module.exports = { getRegisterCandidateAPIAuditLogs, verifyOTPRegistrationAPI, contactUsEmailAPI, registerEmailForgotPasswordAPI, verifyOTPForgotPasswordAPI, passwordResetForgotPasswordAPI };
--- Backend/Routes/auditLogRoutes.js
+++ Backend/Routes/auditLogRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const { getRegisterCandidateAPIAuditLogs, verifyOTPRegistrationAPI } = require('../Controllers/auditLogController');
+const { getRegisterCandidateAPIAuditLogs, verifyOTPRegistrationAPI, contactUsEmailAPI, registerEmailForgotPasswordAPI, verifyOTPForgotPasswordAPI, passwordResetForgotPasswordAPI } = require('../Controllers/auditLogController');
 const router = express.Router();
 
 router.get("/registerCandidateAPI", getRegisterCandidateAPIAuditLogs);
 router.get('/verifyOTPRegistrationAPI', verifyOTPRegistrationAPI);
+
+router.get('/contactUsEmailAPI', contactUsEmailAPI);
+
+router.get("/registerEmailForgotPasswordAPI", registerEmailForgotPasswordAPI);
+router.get("/verifyOTPForgotPasswordAPI", verifyOTPForgotPasswordAPI);
+router.get('/passwordResetForgotPasswordAPI', passwordResetForgotPasswordAPI)
 
 module.exports = router;
