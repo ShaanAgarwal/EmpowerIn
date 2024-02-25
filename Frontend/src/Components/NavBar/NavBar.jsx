7edc5d799de4d23aff32a007b62e8011e2412e69
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-02-25 11:19:08 +0530

    Animated Login Button

--- Frontend/src/Components/NavBar/NavBar.jsx
+++ Frontend/src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import "./NavBar.css";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import CompanyLogo from "../../assets/Images/CompanyLogo/Company-Logo.png";
 
 const NavBar = () => {
-
-  const navigate = useNavigate();
-
-  const navigateLoginPage = async () => {
-    navigate('/login');
-  };
-
   return (
     <div className="navbar-header">
       <div className="navbar-company-logo">
@@ -18,14 +11,16 @@ const NavBar = () => {
           <img src={CompanyLogo} alt="Company Logo" className='company-logo-picture' />
         </Link>
       </div>
-      <div className="nav-content">
-        <Link to="/aboutUs" className="nav-a">About</Link>
-        <Link to="/contactUs" className="nav-a">Contact</Link>
-        <Link to="/services" className="nav-a">Services</Link>
-        <Link to="/career" className="nav-a">Career</Link>
+      <div className="navbar-content">
+        <Link to="/aboutUs" className="navbar-element">About</Link>
+        <Link to="/contactUs" className="navbar-element">Contact</Link>
+        <Link to="/services" className="navbar-element">Services</Link>
+        <Link to="/career" className="navbar-element">Career</Link>
       </div>
-      <div className="nav-button">
-        <button onClick={navigateLoginPage} className="nav-login">Login</button>
+      <div className="navbar-login-button-container">
+        <Link to="/login">
+          <button className="login-button">Login</button>
+        </Link>
       </div>
     </div>
   );
