ba8cf8e40c4df0912e39fa7b3adfc74dbcf27296
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-02-25 11:30:00 +0530

    Added Sliding Animation on Navbar

--- Frontend/src/Components/NavBar/NavBar.jsx
+++ Frontend/src/Components/NavBar/NavBar.jsx
@@ -5,22 +5,24 @@ import CompanyLogo from "../../assets/Images/CompanyLogo/Company-Logo.png";
 
 const NavBar = () => {
   return (
-    <div className="navbar-header">
-      <div className="navbar-company-logo">
-        <Link to="/">
-          <img src={CompanyLogo} alt="Company Logo" className='company-logo-picture' />
-        </Link>
-      </div>
-      <div className="navbar-content">
-        <Link to="/aboutUs" className="navbar-element">About</Link>
-        <Link to="/contactUs" className="navbar-element">Contact</Link>
-        <Link to="/services" className="navbar-element">Services</Link>
-        <Link to="/career" className="navbar-element">Career</Link>
-      </div>
-      <div className="navbar-login-button-container">
-        <Link to="/login">
-          <button className="login-button">Login</button>
-        </Link>
+    <div className="navbar-component">
+      <div className="navbar-header">
+        <div className="navbar-company-logo">
+          <Link to="/">
+            <img src={CompanyLogo} alt="Company Logo" className='company-logo-picture' />
+          </Link>
+        </div>
+        <div className="navbar-content">
+          <Link to="/aboutUs" className="navbar-element">About</Link>
+          <Link to="/contactUs" className="navbar-element">Contact</Link>
+          <Link to="/services" className="navbar-element">Services</Link>
+          <Link to="/career" className="navbar-element">Career</Link>
+        </div>
+        <div className="navbar-login-button-container">
+          <Link to="/login">
+            <button className="login-button">Login</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
