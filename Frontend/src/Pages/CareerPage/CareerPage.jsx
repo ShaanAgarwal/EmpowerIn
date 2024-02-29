802ee2f47844ce50fe0b6e27a96e4484d77d4d64
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-02-29 09:54:01 +0530

    Made the change in the careers page component

--- Frontend/src/Pages/CareerPage/CareerPage.jsx
+++ Frontend/src/Pages/CareerPage/CareerPage.jsx
@@ -4,22 +4,24 @@ import "./CareerPage.css";
 import NavBar from '../../Components/NavBar/NavBar';
 import Footer from '../../Components/Footer/Footer';
 
-// ...
-
 const CareerPage = () => {
   const [allJobs, setAllJobs] = useState([]);
   const [loading, setLoading] = useState(true);
   const [userCategories, setUserCategories] = useState([]);
 
   useEffect(() => {
-    fetchUserCategories();
+    const email = localStorage.getItem('email');
+    if (email) {
+      fetchUserCategories();
+    } else {
+      fetchAllJobs();
+    };
   }, []);
 
   useEffect(() => {
-    // Fetch job posts after user categories are loaded
     if (userCategories.length > 0) {
       fetchAllJobs();
-    }
+    };
   }, [userCategories]);
 
   const fetchUserCategories = async () => {
@@ -28,42 +30,21 @@ const CareerPage = () => {
       const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/candidate/getCategories`, {
         params: { email },
       });
-
       setUserCategories(response.data.categories);
     } catch (error) {
       console.error('Error fetching user categories:', error);
-    }
+    };
   };
 
   const fetchAllJobs = async () => {
     try {
       const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/job/getAllJobs`);
-      const sortedJobs = sortJobsByPriority(response.data.jobs);
-      setAllJobs(sortedJobs);
+      setAllJobs(response.data.jobs);
       setLoading(false);
     } catch (error) {
       console.error('Error fetching all jobs:', error);
       setLoading(false);
-    }
-  };
-
-  const sortJobsByPriority = (jobs) => {
-    // Sort jobs based on user's priority categories
-    const sortedJobs = jobs.sort((jobA, jobB) => {
-      const categoryAIndex = findHighestPriorityCategory(userCategories, jobA.categoryKeywords);
-      const categoryBIndex = findHighestPriorityCategory(userCategories, jobB.categoryKeywords);
-      return categoryAIndex - categoryBIndex;
-    });
-
-    return sortedJobs;
-  };
-
-  const findHighestPriorityCategory = (userCategories, jobCategories) => {
-    // Find the highest priority category index for a job
-    const highestPriorityIndex = Math.min(
-      ...jobCategories.map(category => userCategories.indexOf(category))
-    );
-    return highestPriorityIndex !== -1 ? highestPriorityIndex : Infinity;
+    };
   };
 
   return (
